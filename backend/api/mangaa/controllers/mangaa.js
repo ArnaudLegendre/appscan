296d3912cfdf5b8606ff732d446b06b733725a59
ArnaudLegendre <57393692+ArnaudLegendre@users.noreply.github.com>
2021-04-14 19:41:08 +0200

    auto create rss

--- backend/api/mangaa/controllers/mangaa.js
+++ backend/api/mangaa/controllers/mangaa.js
@@ -8,12 +8,15 @@ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 const axios = require('axios');
 module.exports = {
     async create(ctx) {
-        const rss = await this.fetchRss(ctx.request.body)
+        let url = this.createUrl(ctx.request.body.titre);
+        const rss = await this.fetchRss(url);
         const lastChapter = this.getLastChapter(await rss);
-        const namemanga = this.getName(await rss)
-        const img = await this.getLinkImg(namemanga)
+        
+        const img = await this.getLinkImg(ctx.request.body.titre);
+        // const img = await this.getLinkImg(namemanga);
         ctx.request.body.dernierchapitre = lastChapter;
         ctx.request.body.image = img;
+        ctx.request.body.url = url;
         let entity = await strapi.services.mangaa.create(ctx.request.body);
         return sanitizeEntity(entity, { model: strapi.models.mangaa });
     },
@@ -21,11 +24,6 @@ module.exports = {
         const firstSplit = manga.split("</link>");
         const secondSplit = firstSplit[1].split("/");
         return secondSplit[4];
-    },
-    getName(manga) {
-        const firstSplit = manga.split("</link>");
-        const secondSplit = firstSplit[1].split("/");
-        return secondSplit[3]
     },
     async getLinkImg(manga) {
         const url = `https://kitsu.io/api/edge/manga?filter[text]=${manga}/`;
@@ -40,8 +38,14 @@ module.exports = {
         const img = req.data.data[0].attributes.posterImage.small;
         return img
     },
-    async fetchRss(manga) {
-        const { data } = await axios.get(manga.url);
+    createUrl(manga) {
+        let name = manga.replace(/\s+/g, "-")
+        let url = `https://www.japscan.se/rss/${name}/`
+        return url
+
+    },
+    async fetchRss(url) {
+        const { data } = await axios.get(url);
         return data
     }
 };
