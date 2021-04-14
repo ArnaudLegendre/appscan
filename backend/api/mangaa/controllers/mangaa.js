88ff056b0eac1cbb8f726e21ded1f08cc7032642
ArnaudLegendre <57393692+ArnaudLegendre@users.noreply.github.com>
2021-04-14 18:39:01 +0200

    get img from kitsu

--- backend/api/mangaa/controllers/mangaa.js
+++ backend/api/mangaa/controllers/mangaa.js
@@ -8,16 +8,40 @@ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 const axios = require('axios');
 module.exports = {
     async create(ctx) {
-        const lastChapter = await this.getLastChapter(ctx.request.body);
+        const rss = await this.fetchRss(ctx.request.body)
+        const lastChapter = this.getLastChapter(await rss);
+        const namemanga = this.getName(await rss)
+        const img = await this.getLinkImg(namemanga)
         ctx.request.body.dernierchapitre = lastChapter;
+        ctx.request.body.image = img;
         let entity = await strapi.services.mangaa.create(ctx.request.body);
         return sanitizeEntity(entity, { model: strapi.models.mangaa });
     },
-    async getLastChapter(manga) {
-        const { data } = await axios.get(manga.url);
-        const firstSplit = data.split("</link>");
+    getLastChapter(manga) {
+        const firstSplit = manga.split("</link>");
         const secondSplit = firstSplit[1].split("/");
-        
         return secondSplit[4];
     },
+    getName(manga) {
+        const firstSplit = manga.split("</link>");
+        const secondSplit = firstSplit[1].split("/");
+        return secondSplit[3]
+    },
+    async getLinkImg(manga) {
+        const url = `https://kitsu.io/api/edge/manga?filter[text]=${manga}/`;
+        const option = {
+            headers: {
+                'Accept': 'application/vnd.api+json',
+                'Content-Type': 'application/vnd.api+json'
+            }
+        }
+        let req;
+        req = await axios.get(url, option);
+        const img = req.data.data[0].attributes.posterImage.small;
+        return img
+    },
+    async fetchRss(manga) {
+        const { data } = await axios.get(manga.url);
+        return data
+    }
 };
